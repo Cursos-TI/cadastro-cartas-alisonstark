ccddf98b39febe57b0600ba356963eec73726f10
Alison Caique <58669866+alisonstark@users.noreply.github.com>
2025-12-26 04:40:46 +0000

    Nova remoção (problema de sync)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,16 +5,12 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Testar em que repo esse commit vai ser salvo
-  printf("Desafio Super Trunfo - Países\n");
-
-  // Testar commit a partir do codespace
-  printf("Desafio Aceito\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
+  
 
 return 0;
 } 
